2d98215bbb5beb0cc601bd0bc8fd8395e57290d9
em2046 <em2046@qq.com>
2017-12-18 16:25:44 +0800

    calendar

--- static/js/nova-calendar.js
+++ static/js/nova-calendar.js
@@ -727,7 +727,7 @@
             }
 
             if (typeof options.template == "object") {
-                template = smallTemplate;
+                template = $.extend(true, {}, smallTemplate);
                 for (var temp in options.template) {
                     template[temp] = options.template[temp];
                 }
